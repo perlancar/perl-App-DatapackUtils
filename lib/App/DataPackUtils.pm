d71bbc63294aed568641ef81eee365920ea7695e
perlancar (@pc-office) <perlancar@gmail.com>
2015-06-17 14:13:27 +0700

    Initial commit

---
+++ lib/App/DataPackUtils.pm
@@ -0,0 +1,23 @@
+package App::DataPackUtils;
+
+# DATE
+# VERSION
+
+use 5.010001;
+
+1;
+# ABSTRACT: Collection of CLI utilities related to Module::DataPack
+
+=head1 DESCRIPTION
+
+This distribution provides the following command-line utilities related to
+L<Module::DataPack>:
+
+#INSERT_EXECS_LIST
+
+
+=head1 SEE ALSO
+
+L<Module::DataPack>
+
+=cut
